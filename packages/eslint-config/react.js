87a57db08e86479d062d50648ad439ca6eb15423
Jason Feng <solidzoro@live.com>
2021-10-23 14:09:21 +0800

    fix: fixed eslint deps

--- packages/eslint-config/react.js
+++ packages/eslint-config/react.js
@@ -1,12 +1,15 @@
 /* eslint-disable max-len */
-const EXTENSIONS = ['.ts', '.tsx', '.d.ts', '.js', '.jsx', '.json'];
+const TS_EXTENSIONS = ['.ts', '.tsx', '.d.ts', '.json'];
+const JS_EXTENSIONS = ['.ts', '.tsx', '.d.ts', '.js', '.jsx', '.json'];
 
 module.exports = {
   extends: ['airbnb', 'airbnb-typescript', 'prettier'],
-  plugins: ['react', '@typescript-eslint'],
+  plugins: ['import', 'react', '@typescript-eslint'],
   parser: '@typescript-eslint/parser',
   parserOptions: {
+    requireConfigFile: false,
     sourceType: 'module',
+    allowImportExportEverywhere: true,
     ecmaVersion: 12,
     project: ['tsconfig.json'],
     createDefaultProgram: true,
@@ -14,6 +17,19 @@ module.exports = {
       jsx: true,
     },
   },
+  overrides: [
+    {
+      files: ['**/*.ts?(x)'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+    },
+  ],
   env: {
     browser: true,
     es2021: true,
@@ -24,18 +40,24 @@ module.exports = {
     __PROD__: true,
   },
   settings: {
+    react: {
+      version: 'detect',
+    },
     'import/parsers': {
-      '@typescript-eslint/parser': ['.ts', '.tsx', '.d.ts'],
+      '@typescript-eslint/parser': TS_EXTENSIONS,
     },
     'import/resolver': {
-      node: { extensions: EXTENSIONS },
-      typescript: {},
+      node: { extensions: JS_EXTENSIONS },
+      typescript: {
+        alwaysTryTypes: true,
+      },
     },
-    'import/extensions': EXTENSIONS,
+    'import/extensions': JS_EXTENSIONS,
   },
   //
   // 0 (off) / 1 (warn) / 2 (error)
   rules: {
+    // common
     'max-len': [2, 80],
     'no-console': 0,
     'no-underscore-dangle': 0,
@@ -47,7 +69,7 @@ module.exports = {
       'WithStatement',
     ],
     //
-    //
+    // import
     'import/named': 0,
     'import/prefer-default-export': 0,
     'import/no-mutable-exports': 1, // for mobx displayName by `babel-plugin-add-react-displayname`
@@ -75,14 +97,14 @@ module.exports = {
       },
     ],
     //
-    //
+    // react
     'react-hooks/exhaustive-deps': 0,
     'react/destructuring-assignment': 0,
     'react/require-default-props': 0,
     'react/no-unused-prop-types': 0,
     'react/prop-types': 0,
     //
-    //
+    // ts
     '@typescript-eslint/dot-notation': 0, // for className e.g. `styles['style-name']`
     '@typescript-eslint/no-unused-vars': 0, // for debug temp vars
     '@typescript-eslint/naming-convention': 0,
